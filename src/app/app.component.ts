7c4840eb8fc6b9b51e8959055ab3ad11690b1097
peterleunan1 <arpm081001@gmail.com>
2023-06-11 11:55:08 -0500

    🎉 First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AppStore } from './models/store.model';
+import { Store } from '@ngrx/store';
+import { Theme, TypographyName } from './models/theme.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'dictionary-web-app';
+  theme$: Observable<string> = new Observable();
+  typography$: Observable<TypographyName['name']> = new Observable();
+  constructor(private store: Store<AppStore>) {}
+
+  getThemeAndTypography() {
+    this.theme$ = this.store.select((store: AppStore) => store.theme);
+    this.typography$ = this.store.select(
+      (store: AppStore) => store.typography.name
+    );
+  }
+  // eslint-disable-next-line @angular-eslint/use-lifecycle-interface
+  ngOnInit(): void {
+    this.getThemeAndTypography();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SwitchComponent } from './components/molecules/switch/switch.component';
+import { LogoComponent } from './components/atoms/logo/logo.component';
+import { DropDownComponent } from './components/atoms/drop-down/drop-down.component';
+import { IconComponent } from './components/atoms/icon/icon.component';
+import { HeaderComponent } from './components/organisms/header/header.component';
+import { LayoutComponent } from './components/templates/layout/layout.component';
+import { StoreModule } from '@ngrx/store';
+import { STORE } from './states/app.state';
+import { InputComponent } from './components/molecules/input/input.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SwitchComponent,
+    LogoComponent,
+    DropDownComponent,
+    IconComponent,
+    HeaderComponent,
+    LayoutComponent,
+    InputComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, StoreModule.forRoot(STORE)],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
